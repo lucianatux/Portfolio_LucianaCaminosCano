df7d8d97f278bd90809f5a4f7275b3aa22fd536e
Luciana <lucianatux@yahoo.com.ar>
2022-08-31 19:44:19 -0300

    skills

--- PortfolioLCC/src/app/components/skills/edit-skill.component.ts
+++ PortfolioLCC/src/app/components/skills/edit-skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Habilidades } from 'src/app/model/habilidades';
+import { HabilidadesService } from 'src/app/service/habilidades.service';
 
 @Component({
   selector: 'app-edit-skill',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-skill.component.css']
 })
 export class EditSkillComponent implements OnInit {
+  habilidad: Habilidades = null;
 
-  constructor() { }
+  constructor(private servhab: HabilidadesService, private activatedRouter: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.servhab.detail(id).subscribe(
+      data =>{
+        this.habilidad = data;
+      }, err =>{
+        alert("Error al modificar habilidad en ngOnInit");
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  onUpdate(): void{
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.servhab.update(id, this.habilidad).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err =>{
+         alert("Error al modificar habilidad en onUpdate");
+         this.router.navigate(['']);
+      }
+    )
   }
 
 }
